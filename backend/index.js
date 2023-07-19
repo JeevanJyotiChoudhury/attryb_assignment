213dff6ba0f557ea8553188147749bb7e0cee3b9
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2023-07-19 11:19:08 +0530

    routes for car crud operation created

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const cors = require("cors");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/user.route");
+const { carRouter } = require("./routes/car.route");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/users", userRouter);
+app.use("/cars", carRouter);
 
 app.listen(process.env.port, async () => {
   try {
